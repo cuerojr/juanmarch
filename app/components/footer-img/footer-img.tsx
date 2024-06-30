e4fb8b5fbcb582365d002ad8b5bd8adab8106e09
cuerojr <cannibal5033@gmail.com>
2024-06-30 13:11:47 -0300

    minor fix

--- app/components/footer-img/footer-img.tsx
+++ app/components/footer-img/footer-img.tsx
@@ -11,92 +11,85 @@ export default function FooterImg() {
           </div>
         </div>
         <p className="body text-slate-100 col-start-1 col-end-6">
-          The story behind Exo Ape is one of exploration, creativity and
-          curiosity.
+          Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+          consectetur, adipisci velit.
         </p>
         <div className="divider col-start-1 col-end-13 my-[4vw] bg-slate-100 h-[1px]"></div>
         <ul className="address text-slate-100 text-xs col-start-1 col-end-4">
           <li className="item">
-            <a
-              rel="noopener"
-              target="_blank"
-              href="https://goo.gl/maps/8z56qdL3F85VQmLf9"
-            >
-              Willem II Singel 8<br />
-              6041 HS, Roermond
+            <a rel="noopener" target="_blank" href="#">
+              San Lorenzo 833
               <br />
-              The Netherlands
+              2000 C.P., Rosario
+              <br />
+              Argentina
             </a>
           </li>
-          <li className="item"  >
-            <a
-              href="mailto:hello@exoape.com"
-              className="footer-link"
-               
-            >
-              hello@exoape.com
+          <li className="item">
+            <a href="#" className="footer-link">
+              hello@cristalstudio.com
             </a>
           </li>
         </ul>
-        <ul className="nav text-slate-100 text-xs col-start-4 col-end-6"  >
-          <li className="item"  >
-            <a href="/work" className="footer-link"  >
+        <ul className="nav text-slate-100 text-xs col-start-4 col-end-6">
+          <li className="item">
+            <a href="/work" className="footer-link">
               Work
             </a>
           </li>
-          <li className="item"  >
-            <a href="/studio" className="footer-link"  >
+          <li className="item">
+            <a href="/studio" className="footer-link">
               Studio
             </a>
           </li>
-          <li className="item"  >
-            <a href="/news" className="footer-link"  >
+          <li className="item">
+            <a href="/news" className="footer-link">
               News
             </a>
           </li>
-          <li className="item"  >
-            <a href="/contact" className="footer-link"  >
+          <li className="item">
+            <a href="/contact" className="footer-link">
               Contact
             </a>
           </li>
         </ul>
-        <ul className="social text-slate-100 text-xs col-start-6 col-end-9"  >
-          <li className="item"  >
+        <ul className="social text-slate-100 text-xs col-start-6 col-end-9">
+          <li className="item">
             <a
               href="#"
               target="_blank"
               rel="noreferrer"
-              className="footer-link"               
+              className="footer-link"
             >
               Behance
             </a>
           </li>
-          <li className="item"  >
+          <li className="item">
             <a
               href="#"
               target="_blank"
               rel="noreferrer"
-              className="footer-link"               
+              className="footer-link"
             >
               Dribbble
             </a>
           </li>
-          <li className="item"  >
+          <li className="item">
             <a
               href="#"
               target="_blank"
               rel="noreferrer"
-              className="footer-link"               
+              className="footer-link"
             >
               Twitter
             </a>
           </li>
-          <li className="item"  >
+          <li className="item">
             <a
               href="#"
               target="_blank"
               rel="noreferrer"
-              className="footer-link"               
+              className="footer-link"
             >
               Instagram
             </a>
@@ -118,7 +111,7 @@ export default function FooterImg() {
                   viewBox="0 0 11 10"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="icon-arrow h-[.8vw] w-[.8vw]"                  
+                  className="icon-arrow h-[.8vw] w-[.8vw]"
                 >
                   <path
                     d="M0 5.65612V4.30388L8.41874 4.31842L5.05997 0.95965L5.99054 0L10.9923 4.97273L6.00508 9.96L5.07451 9.00035L8.43328 5.64158L0 5.65612Z"
